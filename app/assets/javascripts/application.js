23da6403153a157438990047b9ffaec780f2784f
Ethan Stiffelman <eg.stiffelman@gmail.com>
2013-07-25 15:30:17 -0700

    added trial gmap js to app.js file (commented out)

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -4,4 +4,22 @@
 //= require_tree .
 //= require_self
 
-$(document).foundation();
+$(document).foundation({
+
+// 		GMaps.geolocate({
+// 	  success: function(position) {
+// 	    map.setCenter(position.coords.latitude, position.coords.longitude);
+// 	  },
+// 	  error: function(error) {
+// 	    alert('Geolocation failed: '+error.message);
+// 	  },
+// 	  not_supported: function() {
+// 	    alert("Your browser does not support geolocation");
+// 	  },
+// 	  always: function() {
+// 	    alert("Done!");
+// 	  }
+	  
+// 	});
+// });
+
